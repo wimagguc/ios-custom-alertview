3a7393e8ea43fdadab736064cc5192f791e3c5ac
Mateusz Dzwonek <mateusz.dzwonek@gmail.com>
2015-02-08 15:20:51 +0000

    Remove hardcoded color. Will use UIAppearance instead.

--- CustomIOS7AlertView/CustomIOS7AlertView/View/CustomIOS7AlertView.m
+++ CustomIOS7AlertView/CustomIOS7AlertView/View/CustomIOS7AlertView.m
@@ -249,8 +249,6 @@ CGFloat buttonSpacerHeight = 0;
         [closeButton setTag:i];
 
         [closeButton setTitle:[buttonTitles objectAtIndex:i] forState:UIControlStateNormal];
-        [closeButton setTitleColor:[UIColor colorWithRed:0.0f green:0.5f blue:1.0f alpha:1.0f] forState:UIControlStateNormal];
-        [closeButton setTitleColor:[UIColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:0.5f] forState:UIControlStateHighlighted];
         [closeButton.titleLabel setFont:[UIFont boldSystemFontOfSize:14.0f]];
         [closeButton.layer setCornerRadius:kCustomIOS7AlertViewCornerRadius];
 
